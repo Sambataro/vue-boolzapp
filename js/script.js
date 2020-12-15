45602cf312cd08680f0eb6e048e863b819d4a43c
fortunato <sambataro1990@gmail.com>
2020-12-15 14:44:43 +0100

    creato dropdown ma da fixare

--- js/script.js
+++ js/script.js
@@ -10,17 +10,19 @@ var app = new Vue({
     			{
     				date: '10/01/2020 15:30:55',
     				text: 'Hai portato a spasso il cane?',
-    				status: 'sent'
+    				status: 'sent',
     			},
     			{
     				date: '10/01/2020 15:50:00',
     				text: 'Ricordati di dargli da mangiare',
-    				status: 'sent'
+    				status: 'sent',
+
     			},
     			{
     				date: '10/01/2020 16:15:22',
     				text: 'Tutto fatto!',
-    				status: 'received'
+    				status: 'received',
+
     			}
     		],
     	},
@@ -32,17 +34,20 @@ var app = new Vue({
     			{
     				date: '20/03/2020 16:30:00',
     				text: 'Ciao come stai?',
-    				status: 'sent'
+    				status: 'sent',
+
     			},
     			{
     				date: '20/03/2020 16:30:55',
     				text: 'Bene grazie! Stasera ci vediamo?',
-    				status: 'received'
+    				status: 'received',
+
     			},
     			{
     				date: '20/03/2020 16:35:00',
     				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-    				status: 'sent'
+    				status: 'sent',
+
     			}
     		],
     	},
@@ -54,17 +59,20 @@ var app = new Vue({
     			{
     				date: '28/03/2020 10:10:40',
     				text: 'La Marianna va in campagna',
-    				status: 'received'
+    				status: 'received',
+
     			},
     			{
     				date: '28/03/2020 10:20:10',
     				text: 'Sicuro di non aver sbagliato chat?',
-    				status: 'sent'
+    				status: 'sent',
+
     			},
     			{
     				date: '28/03/2020 16:15:22',
     				text: 'Ah scusa!',
-    				status: 'received'
+    				status: 'received',
+
     			}
     		],
     	},
@@ -76,12 +84,14 @@ var app = new Vue({
     			{
     				date: '10/01/2020 15:30:55',
     				text: 'Lo sai che ha aperto una nuova pizzeria?',
-    				status: 'sent'
+    				status: 'sent',
+
     			},
     			{
     				date: '10/01/2020 15:50:00',
     				text: 'Si, ma preferirei andare al cinema',
-    				status: 'received'
+    				status: 'received',
+
     			}
     		],
     	},
@@ -89,6 +99,7 @@ var app = new Vue({
     objectIndex: 0,
     inputMessage: "",
     inputFieldRubrica: "",
+    scope: false,
     },
 
     methods: {
@@ -97,7 +108,7 @@ var app = new Vue({
       },
 // -------------------------------------
       addMessage: function() {
-        var backup = {
+        let backup = {
           date: dayjs().format('DD/MM/YYYY H:mm:ss'),
           text: this.inputMessage,
           status: 'sent'
@@ -129,8 +140,16 @@ var app = new Vue({
         }
       );
     },
+// -----------------------------------------------
 
+    toggleChevron: function(objectIndex) {
+        this.selectedMessage = objectIndex;
+        this.scope ? this.scope = false : this.scope = true;
+      },
 
+      // da sistemare mi seleziona tutti i messaggi
+
+// ---------------------------------------------
 
 		}
 });
